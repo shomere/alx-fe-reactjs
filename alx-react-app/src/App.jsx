3cb8ee1f52e62ab31f3cff8b45c3d662d2761851
shomere <s.homere@alustudent.com>
2025-11-01 22:38:55 +0200

    App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div>
-        <WelcomeMessage/>
+        <WelcomeMessage />
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
