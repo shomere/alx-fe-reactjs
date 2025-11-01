da14c0a388abb288bc08cefbcb442f37cdf8bacb
shomere <s.homere@alustudent.com>
2025-11-01 22:18:49 +0200

    correction of modify jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,9 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone, I'm learning react at ALX!</h1>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
-            <p>I'm learning about JSX!</p>
+            <p>I am learning about JSX!</p>
         </div>
     );
 }
