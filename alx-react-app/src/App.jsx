77462dd527194a46e7af24bbd9a62238ca3d59e9
shomere <s.homere@alustudent.com>
2025-11-01 22:10:14 +0200

    modify jsxin a pre-built react components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
