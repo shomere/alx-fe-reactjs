bc581c1b2a7fb88394f5122fef9ce99e8a4450a7
shomere <s.homere@alustudent.com>
2025-12-07 17:40:12 +0200

    Error fixing

--- recipe-sharing-platform/vite.config.js
+++ recipe-sharing-platform/vite.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // You can set it to 'media' or 'class' if needed
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
